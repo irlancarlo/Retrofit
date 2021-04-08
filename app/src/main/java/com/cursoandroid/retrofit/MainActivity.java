f9b149e01e1ee82eb01416377b89ad96fc2afcc2
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-04-08 11:22:02 -0400

    add dependência retrofit

--- app/src/main/java/com/cursoandroid/retrofit/MainActivity.java
+++ app/src/main/java/com/cursoandroid/retrofit/MainActivity.java
@@ -3,12 +3,26 @@ package com.cursoandroid.retrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtRetrofit;
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("viacep.com.br/ws/69073060/json/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        txtRetrofit = findViewById(R.id.txtRetrofit);
     }
 }
